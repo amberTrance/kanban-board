186288d7fa5b455ba55364413580245f5c1114d2
amberTrance <mind.edifice@gmail.com>
2021-06-29 13:16:10 +0300

    Added multiple lists and Delete functionality

--- src/App.js
+++ src/App.js
@@ -1,29 +1,66 @@
 import { useState } from 'react'
 
 function App() {
-  const [inputList, setInputList] = useState([])
+  const [inputList, setInputList] = useState([
+    []
+  ])
+  // [ [a,a,a,a,a], [df,fdf,fdfd,fdfd]]
 
-  const handleAddCard = (e) => {
+  const handleAddCard = (e,i) => {
     const list = [...inputList]
 
-    list.push(e.target.parentNode.firstChild.value)
+    list[i].push(e.target.parentNode.firstChild.value)
+
+    e.target.parentNode.firstChild.value = ''
 
     setInputList(list)
   }
 
+  const handleAddList = (e) => {
+    const list = [...inputList]
+
+    list.push([])
+
+    setInputList(list)
+  }
+
+  const handleDeleteCard = (e,i,ind) => {
+    const list = [...inputList]
+
+    list[i].splice(ind, 1)
+
+    setInputList(list)
+  }
+
+  const handleEditCard = (e,i) => {
+    
+  }
+
   return (
     <div className="App">
-      <div className="list-box">
-        { inputList.length > 0 && inputList.map((card, i) => {
+      <div className="lists">
+        { inputList.map((list, i) => {
           return (
-            <div>{card}</div>
+            <div className="list-box" key={`list${i+1}`}>
+              { list.map((card, ind) => {
+                return (
+                  <div className="card" key={`card${ind+1}`}>
+                    <p>{card}</p>
+                    <button onClick={(e) => handleDeleteCard(e,i,ind)}>Delete</button>
+                    <button onClick={(e) => handleEditCard(e,i,ind)}>Edit</button>
+                  </div>
+                )
+              })}
+
+              <div>
+                <input type="text" placeholder="Add another card"/>
+                <button onClick={(e) => handleAddCard(e,i)}>Add Card</button>
+              </div>
+            </div> 
           )
         })}
-        <div>
-          <input type="text" placeholder="Add another card"/>
-          <button onClick={(e) => handleAddCard(e)}>Add Card</button>
-        </div>
       </div>
+      <button onClick={(e) => handleAddList(e)}>Add New List</button>
     </div>
   )
 }
