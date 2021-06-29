6c4bb8e6df1bcd7537337e015c5b507caec80053
amberTrance <mind.edifice@gmail.com>
2021-06-29 11:58:54 +0300

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
 
 function App() {
+  const [inputList, setInputList] = useState([])
+
+  const handleAddCard = (e) => {
+    const list = [...inputList]
+
+    list.push(e.target.parentNode.firstChild.value)
+
+    setInputList(list)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="list-box">
+        { inputList.length > 0 && inputList.map((card, i) => {
+          return (
+            <div>{card}</div>
+          )
+        })}
+        <div>
+          <input type="text" placeholder="Add another card"/>
+          <button onClick={(e) => handleAddCard(e)}>Add Card</button>
+        </div>
+      </div>
     </div>
-  );
+  )
 }
 
 export default App;
